6cba228f93970dd7f677572ea4532bf24ae837e8
florinerecalt <florine.recalt@gmail.com>
2021-07-15 15:10:30 +0200

    ajout tag et categorie dans la création des articles

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,10 +20,24 @@ class ArticleType extends AbstractType
         $builder
             ->add('title')
             ->add('content')
-            ->add('createdAt')
-            ->add('isPublished')
-//            ->add('category')
-//            ->add('tag')
+            ->add('createdAt', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => "Date de création",    //Pas obligatoire, défini le label par défaut mais mdifiable en twig
+                'data' => new \DateTime('NOW')
+            ])
+            ->add('isPublished', CheckboxType::class, [
+                'data' => true
+            ])
+            ->add('category', EntityType::class, [
+                // Category fait référence à l'entité Category
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
+            ->add('tag', EntityType::class, [
+                // Tag fait référence à l'entité Tag
+                'class' => Tag::class,
+                'choice_label' => 'title'
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
